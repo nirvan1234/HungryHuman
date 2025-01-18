e82613cf5c898e8f8ad04912b53ff3b31d88b9e7
Nirvan Pandagre <nirvanpandagre@Nirvans-MacBook-Air.local>
2025-01-18 17:46:11 +0530

    pagination

--- src/About.js
+++ src/About.js
@@ -1,13 +1,91 @@
-import React, { Component } from 'react'
+
+import React, { useState, useEffect } from 'react';
+
+const About = (props) => {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [noPerPage, setNoPerPage] = useState(10);
 
 
-const About = () =>{
-    return(
-        <div>
-            <h1>about</h1>
-        </div>
-    )
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  const fetchData = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const json = await response.json();
+    // const pageAsPerData = json.slice(intialPageFeed, endPageFeed);
+    setData(json);
+
+  }
+  const paginationForward = (p) => {
+    if(p !== noOfPage) setPage(p => p + 1);
+
+
+  }
+
+  const paginationBacward = (p) => {
+    if( p !==  1){
+        setPage(p => p - 1);
+    }
+    
+  }
+  const noOfPage = data.length/noPerPage;
+
+  const toDisplayPageNo = [...Array(noOfPage +1).keys()].slice(1)
+
+  const lastIndexPage = page * noPerPage;
+  const fistIndexPage = lastIndexPage - noPerPage;
+
+  const displayItems = data.slice(fistIndexPage, lastIndexPage);
+
+  console.log(toDisplayPageNo);
+  return (
+    <div className='App'>
+      <div>
+        {
+          displayItems.map((item, index) => (
+            <div className="bg-yellow-400" key={index}>
+              <h1>{item.id}</h1>
+              <h2>{item.title}</h2>
+              <h2>{item.body}</h2>
+            </div>
+          ))
+        }
+        <button className='bg-slate-500 px-11 mx-72 ' onClick={() => paginationBacward(page)}>Change Page backward</button>
+        {toDisplayPageNo.map((page) =>(
+            <span onClick={()=> setPage(page)}>  {page}   | </span>
+        )
+        )}
+        <button className='bg-slate-500 px-11 mx-10' onClick={() => paginationForward(page)}>Change Page forward</button>
+      </div>
+    </div>
+  );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react'
+
+
+// const About = () =>{
+//     return(
+//         <div>
+//             <h1>about</h1>
+//         </div>
+//     )
+// }
+
 
 // class About extends React.Component{
 //     constructor(props){
